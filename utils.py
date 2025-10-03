d0154c815c094d9996061501a1a9642401934f09
TK8877 <145332638+TK8877@users.noreply.github.com>
2025-10-03 03:23:27 +0000

    created load create key func

--- utils.py
+++ utils.py
@@ -1,1 +1,16 @@
+import os, json
+from cryptography.fernet import Fernet
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from datetime import datetime
+
+_KEYFILE = "secret.key"
+
+def load_create_key(path = _KEYFILE):
+    if os.path.exists(path):
+        with open(path, "rb") as f:
+            return f.read()
+    key = Fernet.generate_key()
+    with open(path, "rb") as f:
+        f.write(key)
+    return key
 
